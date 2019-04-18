b5a20dd732f0a1bfdf26538b11b2da0d0d170da3
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-04-18 16:12:22 -0500

    hey

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Feature from "./Feature";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Feature />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import store, { REMOVE_ITEM } from "./store";
+import Item from "./item";
+
+class List extends Component {
+  state = {
+    todoItems: store.getState().todoItems
+  };
+  componentDidMount() {
+    store.subscribe(() => {
+      console.log("AAAA", store.getState().todoItems);
+      this.setState({
+        todoItems: store.getState().todoItems
+      });
+    });
+  }
+  handleDelete(index) {
+    store.dispatch({
+      type: REMOVE_ITEM,
+      index
+    });
+  }
+
+  render() {
+    console.log(this.state.todoItems);
+
+    return (
+      <div>
+        {this.state.todoItems.map(val => {
+          return <Item text={val} handleDelete={this.handleDelete} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default List;
